a5a197e38bbb081ec0be80d0aa43547917cceb10
Adam Ronald Gray <82571100+agray998@users.noreply.github.com>
2023-12-07 15:42:21 +0000

    Create SeleniumTests.java

---
+++ src/com/qa/selenium/SeleniumTests.java
@@ -0,0 +1,22 @@
+package com.qa.selenium;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SeleniumTests {
+    private String DRIVER_PATH = "";
+    private String BASE_URL = "http://localhost/home";
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", this.DRIVER_PATH); // <-- Change this path
+        WebDriver driver = new ChromeDriver();
+        String expectedTitle = "ToDo List - Home";
+        String actualTitle = "";
+        driver.get(this.BASE_URL);
+        actualTitle = driver.getTitle();
+        if (actualTitle.contentEquals(expectedTitle)){
+            System.out.println("TEST PASSED!");
+        } else {
+            System.out.println("TEST FAILED");
+        }
+        driver.close();
+    }
+}
