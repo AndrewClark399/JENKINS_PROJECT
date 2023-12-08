16c3255d95671313bf4e0aeafe182852794fe6ba
Adam Ronald Gray <82571100+agray998@users.noreply.github.com>
2023-12-08 10:36:52 +0000

    Update SeleniumTests.java

--- src/main/java/com/qa/selenium/SeleniumTests.java
+++ src/main/java/com/qa/selenium/SeleniumTests.java
@@ -1,13 +1,14 @@
 package com.qa.selenium;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.By;
 
 public class SeleniumTests {
-    private String DRIVER_PATH = "";
-    private String BASE_URL = "http://localhost/home";
+    private String DRIVER_PATH = "C:\Program Files\edgedriver\msedgedriver.exe";
+    private String BASE_URL = "http://localhost:3000/home";
     public static void main(String[] args) {
-        System.setProperty("webdriver.chrome.driver", this.DRIVER_PATH);
-        WebDriver driver = new ChromeDriver();
+        System.setProperty("webdriver.edge.driver", this.DRIVER_PATH);
+        WebDriver driver = new EdgeDriver();
         String expectedTitle = "ToDo List - Home";
         String actualTitle = "";
         driver.get(this.BASE_URL);
